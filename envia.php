48531bccc342f1a342b6ebb56cf66352ca62e976
alexalexandre1989@hotmail.com <alexalexandre1989@hotmail.com>
2024-09-29 18:35:39 -0300

    feito alterações

---
+++ envia.php
@@ -0,0 +1,18 @@
+<?php
+
+    $nome = addcslashes($_POST['NumeroA']);
+    $email = addcslashes($_POST['NumeroB']);
+
+    $para = "alexlexadre1989@hotmail.com";
+    $assunto = "Validação com JavaScript";
+
+    $corpo = "NumeroA: ".$NumeroA."\n"."NumeroB: ".$NumeroB."\n"
+
+    $cabeca = "From: alex.alexandre@mdgeo.com.br"."\n"."Reply-to: "$email."\n"."X=Mailer:PHP/".phpversion();
+
+    if(mail($para,$assunto,$corpo,$cabeca)){
+        echo("E-mail enviado com sucesso!");
+    }else{
+        echo("Houve um erro ao enviar o email");
+    }
+?>
